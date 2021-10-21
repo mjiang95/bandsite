5298959445ed80e06a36aea91bb0c52b5ff5a281
mjiang95 <minniejiang2013@gmail.com>
2021-10-21 01:47:44 -0700

    Updated my biography page: convert timestampinto date, and sort comments from API according to date comment was posted.

--- scripts/index.js
+++ scripts/index.js
@@ -1,125 +1,127 @@
-let commentSection= document.querySelector(".comments-section");
+let commentSection = document.querySelector(".comments-section");
+const API_KEY_STRING = "?api_key=5982046c-40a3-48ae-8614-23aea4d1a7e9";
+const userCommentUrl = `https://project-1-api.herokuapp.com/comments${API_KEY_STRING}`;
+console.log(userCommentUrl);
 
-let commentsArray = [{
-
-    name: "Connor Walton",
-    comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserve reverence. Let us appreciate this for what it is and what it contains.",
-    date: "02/17/2021"
-    
-},
-{   
-    name: "Emilie Beach", 
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-    date: "01/09/2021"
-    
-},
-{
-    name: "Miles Acosta",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What beautiful expression of creativity. Can't get enough.",
-    date: "12/20/2021"
-    
-}];
-
-let parent = document.querySelector('.comments-section');
-
-
+let parent = document.querySelector(".comments-section");
 
 let displayArrayObjects = (comments) => {
-    
-let commentsDivider = document.createElement("div");
-commentsDivider.classList.add("comments__divider");
+  let commentsDivider = document.createElement("div");
+  commentsDivider.classList.add("comments__divider");
 
+  let avatar = document.createElement("Image");
+  avatar.classList.add("comments-avatar");
 
-let avatar = document.createElement("Image");
-avatar.classList.add("comments-avatar");
+  let commentsSubDivider = document.createElement("div");
+  commentsSubDivider.classList.add("comments__sub-divider");
 
-// avatar.innerText =
+  let personName = document.createElement("h2");
+  personName.classList.add("comments__name");
+  personName.innerText = comments.name;
 
+  let personComment = document.createElement("h3");
+  personComment.classList.add("person__comments");
+  personComment.innerText = comments.comment;
 
-let commentsSubDivider = document.createElement("div");
-commentsSubDivider.classList.add("comments__sub-divider");
+  let commentTime = document.createElement("p");
+  commentTime.classList.add("comment-Time");
 
-let personName = document.createElement("h2");
-personName.classList.add("comments__name");
-personName.innerText = comments.name;
+    let timestamp = comments.timestamp; 
+    let dateObj = new Date(timestamp);
+    let month = dateObj.getMonth() + 1;
+    let Year = dateObj.getFullYear();
+    let date = dateObj.getDate();
 
-let personComment = document.createElement("h3");
-personComment.classList.add("person__comments");
-personComment.innerText = comments.comment;
+    commentTime.innerText = `${month}/${date}/${Year}`;
+//   commentTime.innerText = comments.timestamp;
 
-let commentTime = document.createElement("p");
-commentTime.classList.add("comment-Time");
-commentTime.innerText = comments.date;
+  commentsSubDivider.appendChild(personName);
+  commentsSubDivider.appendChild(commentTime);
 
-commentsSubDivider.appendChild(personName);
-commentsSubDivider.appendChild(commentTime);
+  let commentsContainer = document.createElement("div");
+  commentsContainer.classList.add("comments-container");
 
-// commentsDivider.appendChild(commentsSubDivider);
-// commentsDivider.appendChild(personComment);
+  commentsContainer.appendChild(commentsSubDivider);
+  commentsContainer.appendChild(personComment);
 
-let commentsContainer = document.createElement("div");
-commentsContainer.classList.add("comments-container");
+  commentsDivider.appendChild(avatar);
+  commentsDivider.appendChild(commentsContainer);
 
-commentsContainer.appendChild(commentsSubDivider);
-commentsContainer.appendChild(personComment);
-
-commentsDivider.appendChild(avatar);
-commentsDivider.appendChild(commentsContainer);
-
-
-parent.appendChild(commentsDivider);
-
+  parent.appendChild(commentsDivider);
 };
 
-    commentsArray.forEach(object => {
-        displayArrayObjects(object);
-    })
+axios.get(userCommentUrl).then((result) => {
+  console.log(result.data);
+
+  let defaultComment = result.data;
+    console.log(defaultComment);
+    defaultComment.forEach((comments) => {
+    const sortedActivities = result.data.slice().sort((a, b) => b.timestamp - a.timestamp);
+    console.log(sortedActivities);
+    displayArrayObjects(comments);
+  });
 
 
+});
 
 let formEl = document.querySelector("form");
 console.log(formEl);
 
-
+// let newTimeConverter = comments.map((element) => )
 function getFormattedDate() {
     const currentDate = new Date();
-    return `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-}
+    return `${
+      currentDate.getMonth() + 1
+    }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+  }
 
 formEl.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let userName = e.target.name.value;
-    let userComment = e.target.response.value; 
-    let userDate = getFormattedDate();
+  let userName = e.target.name.value;
+  let userComment = e.target.response.value;
+  let userDate = getFormattedDate();
 
-    console.log(userName, userComment, userDate);
-
-        let newComment = {
-
-         name: userName, 
-         comment: userComment, 
-         date: userDate,
-
-        }
-
-    commentSection.innerText= "";
-
-    commentsArray.unshift(newComment);
-    console.log(commentsArray)
-
-    commentsArray.forEach(comment => {
-    displayArrayObjects(comment);
+  axios
+    .post(userCommentUrl, {
+      name: userName,
+      comment: userComment,
+      // "date": userDate
     })
-    
-    formEl.reset();
-    
-})
 
-const API_KEY_STRING = "?api_key=5982046c-40a3-48ae-8614-23aea4d1a7e9";
+    .then((result) => {
+      console.log(result.data);
+      result.data
+        result.data.forEach((comment) => {
+          displayArrayObjects(comment);
 
-axois 
-    .get(`https://project-1-api.herokuapp.com/register)${API_KEY_STRING}`)
-    .then (result => {
-        result.data
+          // axios
+          //   .get(userCommentUrl)
+          //   .then((result) => {
+          //       console.log(result.data);
+          //       result.data.forEach((comment) => {
+          //       displayArrayObjects(comment);
+          //       // result.data.unshift(-1);
+          //       });
+
+          // let comments = result.data;
+          // console.log(comments);
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
     });
+
+  commentSection.innerText = "";
+
+  formEl.reset();
+});
+
+
+// let newArrayDateConverted = comments.map((element) =>{
+
+//     element.timestamp = dateConvert(element.timestamp);
+
+// });
+// console.log(newArrayDateConverted);
