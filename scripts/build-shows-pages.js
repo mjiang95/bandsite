6953950ce5d89d874d183ea0cef511bdeb1d83c1
mjiang95 <minniejiang2013@gmail.com>
2021-10-24 10:39:53 -0700

    Update sprint-2 feedback and include indentations

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -2,106 +2,104 @@ let parent = document.querySelector('.ticket-purchase');
 const API_KEY_STRING = "?api_key=1996a410-381a-4a15-948d-b8d336a28691";
 const showDateURL = `https://project-1-api.herokuapp.com/showdates${API_KEY_STRING}`; 
 
-    let createShowsTitle = document.createElement('h2'); 
-    createShowsTitle.innerText = 'Show'; 
-    parent.appendChild(createShowsTitle);
+let createShowsTitle = document.createElement('h2'); 
+createShowsTitle.innerText = 'Show'; 
+parent.appendChild(createShowsTitle);
 
-    let createTicketPurchaseDiv = document.createElement("div");
-    createTicketPurchaseDiv.classList.add("ticket-purchase__div");
-    parent.appendChild(createTicketPurchaseDiv);
+let createTicketPurchaseDiv = document.createElement("div");
+createTicketPurchaseDiv.classList.add("ticket-purchase__div");
+parent.appendChild(createTicketPurchaseDiv);
 
-    let createDate = document.createElement("h3");
-    createDate.classList.add("ticket-purchase__subheader");
-    createDate.innerText = "Date";
+let createDate = document.createElement("h3");
+createDate.classList.add("ticket-purchase__subheader");
+createDate.innerText = "Date";
 
-    let createVenue = document.createElement("h3");
-    createVenue.classList.add("ticket-purchase__subhearder");
-    createVenue.innerText = "Venue";
+let createVenue = document.createElement("h3");
+createVenue.classList.add("ticket-purchase__subhearder");
+createVenue.innerText = "Venue";
 
-    let createLocation = document.createElement("h3");
-    createLocation.classList.add("ticket-purchase__subheader");
-    createLocation.innerText = "Location";
+let createLocation = document.createElement("h3");
+createLocation.classList.add("ticket-purchase__subheader");
+createLocation.innerText = "Location";
 
-    createTicketPurchaseDiv.appendChild(createDate);
-    createTicketPurchaseDiv.appendChild(createVenue);
-    createTicketPurchaseDiv.appendChild(createLocation);
+createTicketPurchaseDiv.appendChild(createDate);
+createTicketPurchaseDiv.appendChild(createVenue);
+createTicketPurchaseDiv.appendChild(createLocation);
 
-    let createContainer = document.createElement("div");
-    createContainer.classList.add("ticket-purchase__container");
-    createContainer.appendChild(createTicketPurchaseDiv);
+let createContainer = document.createElement("div");
+createContainer.classList.add("ticket-purchase__container");
+createContainer.appendChild(createTicketPurchaseDiv);
 
 let displayTicketInfo = (ticketInfo) => {
 
-    let createShowsInfo = document.createElement('div');
-    createShowsInfo.classList.add("shows-info");
-   
-    let createShowsInfoSubtitle = document.createElement('h3');
-    createShowsInfoSubtitle.classList.add("shows-info__subtitle");
-    createShowsInfoSubtitle.innerText = 'Date';
+let createShowsInfo = document.createElement('div');
+createShowsInfo.classList.add("shows-info");
 
-    createShowsInfo.appendChild(createShowsInfoSubtitle);
+let createShowsInfoSubtitle = document.createElement('h3');
+createShowsInfoSubtitle.classList.add("shows-info__subtitle");
+createShowsInfoSubtitle.innerText = 'Date';
 
-    let createShowsInfoDate = document.createElement('p');
-    createShowsInfoDate.classList.add("shows-info__date");
+createShowsInfo.appendChild(createShowsInfoSubtitle);
 
-    let timestamp = Number(ticketInfo.date); 
-    let dateObj = new Date(timestamp);
-    var  months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-    let month = months[dateObj.getMonth()];
-    var days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
-    let day = days[dateObj.getDay()]
-    let Year = dateObj.getFullYear();
-    let date = dateObj.getDate();
+let createShowsInfoDate = document.createElement('p');
+createShowsInfoDate.classList.add("shows-info__date");
 
-    createShowsInfoDate.innerText = [day+" "+ month +" "+ date +" "+ Year];
+let timestamp = Number(ticketInfo.date); 
+let dateObj = new Date(timestamp);
+var  months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+let month = months[dateObj.getMonth()];
+var days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
+let day = days[dateObj.getDay()]
+let Year = dateObj.getFullYear();
+let date = dateObj.getDate();
 
-    createShowsInfo.appendChild(createShowsInfoDate);
+createShowsInfoDate.innerText = [day+" "+ month +" "+ date +" "+ Year];
 
-    let createShowsInfoSubtitle1 = document.createElement('h3');
-    createShowsInfoSubtitle1.classList.add("shows-info__subtitle");
-    createShowsInfoSubtitle1.innerText = 'Venue';
+createShowsInfo.appendChild(createShowsInfoDate);
 
-    createShowsInfo.appendChild(createShowsInfoSubtitle1);
+let createShowsInfoSubtitle1 = document.createElement('h3');
+createShowsInfoSubtitle1.classList.add("shows-info__subtitle");
+createShowsInfoSubtitle1.innerText = 'Venue';
 
-    let createShowsInfoVenue = document.createElement('p');
-    createShowsInfoVenue.classList.add("shows-info__venue");
-    createShowsInfoVenue.innerText = ticketInfo.place;
+createShowsInfo.appendChild(createShowsInfoSubtitle1);
 
-    createShowsInfo.appendChild(createShowsInfoVenue);
+let createShowsInfoVenue = document.createElement('p');
+createShowsInfoVenue.classList.add("shows-info__venue");
+createShowsInfoVenue.innerText = ticketInfo.place;
 
-    let createShowsInfoSubtitle2 = document.createElement('h3');
-    createShowsInfoSubtitle2.classList.add("shows-info__subtitle");
-    createShowsInfoSubtitle2.innerText = 'Location';
+createShowsInfo.appendChild(createShowsInfoVenue);
 
-    createShowsInfo.appendChild(createShowsInfoSubtitle2);
+let createShowsInfoSubtitle2 = document.createElement('h3');
+createShowsInfoSubtitle2.classList.add("shows-info__subtitle");
+createShowsInfoSubtitle2.innerText = 'Location';
 
-    let createShowsInfoLocation = document.createElement('p');
-    createShowsInfoLocation.classList.add("shows-info__location");
-    createShowsInfoLocation.innerText = ticketInfo.location;
+createShowsInfo.appendChild(createShowsInfoSubtitle2);
 
-    createShowsInfo.appendChild(createShowsInfoLocation);
+let createShowsInfoLocation = document.createElement('p');
+createShowsInfoLocation.classList.add("shows-info__location");
+createShowsInfoLocation.innerText = ticketInfo.location;
 
-    let createButton = document.createElement("button");
-    createButton.classList.add("shows-info__button");
-    createButton.innerText = 'BUY TICKETS';
+createShowsInfo.appendChild(createShowsInfoLocation);
 
-    createShowsInfo.appendChild(createButton);
-    createContainer.appendChild(createShowsInfo);
+let createButton = document.createElement("button");
+createButton.classList.add("shows-info__button");
+createButton.innerText = 'BUY TICKETS';
+
+createShowsInfo.appendChild(createButton);
+createContainer.appendChild(createShowsInfo);
 
 
-    parent.appendChild(createContainer);
-    
-    createShowsInfo.addEventListener("mouseenter", (e) => {
-        createShowsInfo.classList.add("shows-info--hover");
-    })
+parent.appendChild(createContainer);
 
-    createShowsInfo.addEventListener("mouseleave", (e) => {
-        createShowsInfo.classList.remove("shows-info--hover");
-    })
+createShowsInfo.addEventListener("mouseenter", (e) => {
+    createShowsInfo.classList.add("shows-info--hover");
+})
 
-    let ticketPurchase = document.querySelector(".ticket-purchase__container")
+createShowsInfo.addEventListener("mouseleave", (e) => {
+    createShowsInfo.classList.remove("shows-info--hover");
+})
 
-    createShowsInfo.addEventListener("click", (e) => {
+createShowsInfo.addEventListener("click", (e) => {
     createShowsInfo.classList.toggle("shows-info--active");
     })
 };
@@ -110,14 +108,8 @@ let displayTicketInfo = (ticketInfo) => {
 axios
     .get(showDateURL)
     .then((result) => {
-    let defaultComment = result.data;
-    defaultComment.forEach(object => {
-    displayTicketInfo(object);
+        let defaultComment = result.data;
+        defaultComment.forEach(object => {
+        displayTicketInfo(object);
     })
 });
-
-// let ticketInfoCard = document.querySelector(".shows-info");
-
-// ticketInfoCard.addEventListener("click", (e) => {
-//     ticketInfoCard.style.backgroundColor = "grey";
-// })
--- scripts/index.js
+++ scripts/index.js
@@ -1,111 +1,109 @@
 let commentSection = document.querySelector(".comments-section");
 const API_KEY_STRING = "?api_key=5982046c-40a3-48ae-8614-23aea4d1a7e9";
 const userCommentUrl = `https://project-1-api.herokuapp.com/comments${API_KEY_STRING}`;
-console.log(userCommentUrl);
+
 
 let parent = document.querySelector(".comments-section");
 
 let displayArrayObjects = (comments) => {
-  let commentsDivider = document.createElement("div");
-  commentsDivider.classList.add("comments__divider");
+let commentsDivider = document.createElement("div");
+commentsDivider.classList.add("comments__divider");
 
-  let avatar = document.createElement("Image");
-  avatar.classList.add("comments-avatar");
+let avatar = document.createElement("Image");
+avatar.classList.add("comments-avatar");
 
-  let commentsSubDivider = document.createElement("div");
-  commentsSubDivider.classList.add("comments__sub-divider");
+let commentsSubDivider = document.createElement("div");
+commentsSubDivider.classList.add("comments__sub-divider");
 
-  let personName = document.createElement("h2");
-  personName.classList.add("comments__name");
-  personName.innerText = comments.name;
+let personName = document.createElement("h2");
+personName.classList.add("comments__name");
+personName.innerText = comments.name;
 
-  let personComment = document.createElement("h3");
-  personComment.classList.add("person__comments");
-  personComment.innerText = comments.comment;
+let personComment = document.createElement("h3");
+personComment.classList.add("person__comments");
+personComment.innerText = comments.comment;
 
-  let commentTime = document.createElement("p");
-  commentTime.classList.add("comment-Time");
+let commentTime = document.createElement("p");
+commentTime.classList.add("comment-Time");
 
-    let timestamp = comments.timestamp; 
-    let dateObj = new Date(timestamp);
-    let month = dateObj.getMonth() + 1;
-    let Year = dateObj.getFullYear();
-    let date = dateObj.getDate();
+let timestamp = comments.timestamp; 
+let dateObj = new Date(timestamp);
+let month = dateObj.getMonth() + 1;
+let Year = dateObj.getFullYear();
+let date = dateObj.getDate();
 
-    commentTime.innerText = `${month}/${date}/${Year}`;
+commentTime.innerText = `${month}/${date}/${Year}`;
 
-  commentsSubDivider.appendChild(personName);
-  commentsSubDivider.appendChild(commentTime);
+let likeEl = document.createElement("p")
+likeEl.innerText = comments.likes;
 
-  let commentsContainer = document.createElement("div");
-  commentsContainer.classList.add("comments-container");
+commentsSubDivider.appendChild(personName);
+commentsSubDivider.appendChild(commentTime);
 
-  commentsContainer.appendChild(commentsSubDivider);
-  commentsContainer.appendChild(personComment);
 
-  commentsDivider.appendChild(avatar);
-  commentsDivider.appendChild(commentsContainer);
+let commentsContainer = document.createElement("div");
+commentsContainer.classList.add("comments-container");
 
-  parent.appendChild(commentsDivider);
+commentsContainer.appendChild(commentsSubDivider);
+commentsContainer.appendChild(personComment);
+commentsContainer.appendChild(likeEl);
+
+
+commentsDivider.appendChild(avatar);
+commentsDivider.appendChild(commentsContainer);
+
+
+
+parent.appendChild(commentsDivider);
 };
 
-axios.get(userCommentUrl).then((result) => {
-    console.log(result.data);
-
+axios
+  .get(userCommentUrl)
+  .then((result) => {
     let defaultComment = result.data;
-    console.log(defaultComment);
     const sortedComments = result.data.slice().sort((a, b) => b.timestamp - a.timestamp);
-    console.log(sortedComments);
-
     sortedComments.forEach((comment) => {
     displayArrayObjects(comment);
   });
-
-
 });
 
 let formEl = document.querySelector("form");
-console.log(formEl);
 
 function getFormattedDate() {
-    const currentDate = new Date();
-    return `${
-      currentDate.getMonth() + 1
-    }/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-  }
+const currentDate = new Date();
+return `${
+  currentDate.getMonth() + 1
+}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+}
 
 formEl.addEventListener("submit", (e) => {
-  e.preventDefault();
+e.preventDefault();
 
-  let userName = e.target.name.value;
-  let userComment = e.target.response.value;
-  let userDate = getFormattedDate();
+let userName = e.target.name.value;
+let userComment = e.target.response.value;
+let userDate = getFormattedDate();
 
-  axios
+axios
     .post(userCommentUrl, {
-      name: userName,
-      comment: userComment
+    name: userName,
+    comment: userComment
     })
 
     .then((result) => {
 
-    axios
-        .get(userCommentUrl)
-        .then((result) => {
-          const sortedComments = result.data.slice().sort((a, b) => b.timestamp - a.timestamp);
-          console.log(sortedComments);
-
-          sortedComments.forEach((comment) => {
-          displayArrayObjects(comment);
-        })
-
-        .catch((error) => {
-          console.log(error);
-        });
-    });
-
-  commentSection.innerText = "";
-
-  formEl.reset();
+axios
+    .get(userCommentUrl)
+    .then((result) => {
+      const sortedComments = result.data.slice().sort((a, b) => b.timestamp - a.timestamp);
+      sortedComments.forEach((comment) => {
+      displayArrayObjects(comment);
     })
+    .catch((error) => {
+    });
+});
+
+commentSection.innerText = "";
+
+formEl.reset();
+})
 });
